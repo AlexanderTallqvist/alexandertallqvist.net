d0fa79a34b3c6574ae94db7868fa95a60b190934
Alexander Tallqvist <alexander.tallqvist@karhuhelsinki.fi>
2020-08-03 16:44:53 +0300

    ArlMal<3.

---
+++ arlmal/js/main.js
@@ -0,0 +1,34 @@
+
+var startDateTime = new Date(2020,3,6,18,0,0,0);
+var startStamp = startDateTime.getTime();
+
+var newDate = new Date();
+var newStamp = newDate.getTime();
+
+var timer;
+
+
+function updateClock() {
+
+  newDate = new Date();
+  newStamp = newDate.getTime();
+
+  var diff = Math.round((newStamp-startStamp)/1000);
+
+  var d = Math.floor(diff/(24*60*60));
+  diff = diff-(d*24*60*60);
+
+  var h = Math.floor(diff/(60*60));
+  diff = diff-(h*60*60);
+
+  var m = Math.floor(diff/(60));
+  diff = diff-(m*60);
+
+  var s = diff;
+
+  document.getElementById("time-ago-days").innerText = d;
+  document.getElementById("time-ago-hours").innerHTML = m;
+  document.getElementById("time-ago-seconds").innerHTML = s;
+}
+
+setInterval(updateClock, 1000);
